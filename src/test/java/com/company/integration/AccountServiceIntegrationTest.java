5f7823701a2891e26cff71d8147a3ddd19233d85
nijat <khanalievn@gmail.com>
2025-10-17 22:31:50 +0400

    structuce created

--- src/test/java/com/company/integration/AccountServiceIntegrationTest.java
+++ src/test/java/com/company/integration/AccountServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -24,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Testcontainers
+@ActiveProfiles("test")
 public class AccountServiceIntegrationTest {
 
     @Container
